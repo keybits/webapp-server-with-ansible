3dcbdf40e39cbbe0baaf5b41a40b4beea79eeac7
Tom Atkins <tom@knitatoms.net>
2013-06-09 11:22:29 +0100

    Initial config

---
+++ hello_flask/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'postgres://testuser:test1ng@localhost/vagrant_ansible_db'
+
+db = SQLAlchemy(app)
+
+class AppUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+@app.route('/')
+def index():
+    return render_template('index.html', users=AppUser.query.all())
+
+
+@app.before_first_request
+def invalidate_data():
+    db.drop_all()
+    db.create_all()
+    for u, e in [('matt', 'matt@nobien.net'),
+                 ('marc', 'marc@nobien.net')]:
+        db.session.add(AppUser(u, e))
+    db.session.commit()
